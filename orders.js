281177348fd481c94fdcbfefa7d028dc777e7d97
sajad <55943862+Sajadssa@users.noreply.github.com>
2022-03-25 00:34:22 +0430

    CSS AND JS

---
+++ orders.js
@@ -0,0 +1,31 @@
+const orders = [{
+    productName: "Foldable Mini Drone",
+    productNumber: "856321",
+    paymentStatus: "Due",
+    shipping: "Pending"
+},
+{
+    productName: "LAVENDER KF102G Drone",
+    productNumber: "36124",
+    paymentStatus: "Due",
+    shipping: "Pending"
+},
+{
+    productName: "Rok F11 Pro Drone",
+    productNumber: "14562",
+    paymentStatus: "Due",
+    shipping: "Delivered"
+},
+{
+    productName: "GPS 4K Drone",
+    productNumber: "21456",
+    paymentStatus: "Paid",
+    shipping: "Delivered"
+},
+{
+    productName: "LG TV 55 inch 8500 pixel",
+    productNumber: "1456",
+    paymentStatus: "Due",
+    shipping: "Pending"
+}
+];
